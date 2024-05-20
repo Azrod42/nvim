f0b6ce1065eb67b4a353f75cb5d205721abffd40
Tom Sorabella <tsorabella@smartandconnective.com>
2024-05-20 15:21:25 +0200

    feat: update leap config

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -4,5 +4,28 @@ return {
     local leap = require 'leap'
     leap.add_default_mappings()
     leap.opts.case_sensitive = true
+
+    vim.keymap.set({ 'n', 'x', 'o', 'v' }, 't', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o', 'v' }, 'T', '<Plug>(leap-backward)')
+    vim.keymap.set({ 'n', 'x', 'o', 'v' }, 'ts', '<Plug>(leap-from-window)')
+
+    vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' }) -- or some grey
+    vim.api.nvim_set_hl(0, 'LeapMatch', {
+      fg = 'white',
+      bold = true,
+      nocombine = true,
+    })
+
+    vim.api.nvim_set_hl(0, 'LeapLabelPrimary', {
+      fg = 'red',
+      bold = true,
+      nocombine = true,
+    })
+    vim.api.nvim_set_hl(0, 'LeapLabelSecondary', {
+      fg = 'blue',
+      bold = true,
+      nocombine = true,
+    })
+    require('leap').opts.highlight_unlabeled_phase_one_targets = true
   end,
 }
