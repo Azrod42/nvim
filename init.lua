54848b5550e23f9cc668a76e3cf65771a15d26a7
Tom Sorabella <tsorabella@smartandconnective.com>
2024-09-17 14:03:17 +0200

    fix: mason and witch-key

--- init.lua
+++ init.lua
@@ -368,16 +368,28 @@ require('lazy').setup({
       },
 
       -- Document existing key chains
-      spec = {
-        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+      require('which-key').add {
+
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
         { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>h', group = 'Git [H]unk' },
+        { '<leader>h_', hidden = true },
         { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
         { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>s_', hidden = true },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-      },
-    },
+        { '<leader>t_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
+      }
+      -- visual mode
+      require('which-key').add({
+        { '', desc = '<leader>h', mode = 'v' },
+      }, { mode = 'v' })
+    end,
   },
 
   -- NOTE: Plugins can specify dependencies.
@@ -671,7 +683,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
